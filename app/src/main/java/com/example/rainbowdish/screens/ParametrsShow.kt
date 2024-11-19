2e7eaae80c4875cfce820c23821a5e46fbd87b28
M1nX2 <Zhenya23082003A@gmail.com>
2024-11-19 16:18:20 +0500

    added another screens

---
+++ app/src/main/java/com/example/rainbowdish/screens/ParametrsShow.kt
@@ -0,0 +1,19 @@
+package com.example.rainbowdish.screens
+
+import android.os.Bundle
+import com.example.rainbowdish.R
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+class ParametrsShow: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.parametrs_show)
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rrii3fccqqw))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rugh8oi5akh8))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rq67t23gde9r))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rh1t72hr6gs7))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r2beq0v9iv9))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r0ay96i5ez9ac))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rimpywthfktm))
+    }
+}
